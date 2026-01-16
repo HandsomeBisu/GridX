efb6238ed4c9e71e59bc3e564e5f670af2f4c8b9
HandsomeBisu <joscar12@daum.net>
2026-01-16 19:41:49 +0900

    Add files via upload

--- utils/sound.ts
+++ utils/sound.ts
@@ -1,12 +1,12 @@
 // Simple Sound Utility using HTML5 Audio
 export const SOUNDS = {
-  CLICK: 'https://assets.mixkit.co/active_storage/sfx/2571/2571-preview.mp3', // Pop click
-  DICE_ROLL: 'https://assets.mixkit.co/active_storage/sfx/2003/2003-preview.mp3', // Rolling
-  MOVE: 'https://assets.mixkit.co/active_storage/sfx/2572/2572-preview.mp3', // Short tick
+  CLICK: 'https://assets.dpsteam.xyz/gridx-assets/tick.mp3', // Pop click
+  DICE_ROLL: 'https://assets.dpsteam.xyz/gridx-assets/dice.mp3', // Rolling
+  MOVE: 'https://assets.dpsteam.xyz/gridx-assets/tick.mp3', // Short tick
   BUILD: 'https://assets.mixkit.co/active_storage/sfx/95/95-preview.mp3', // Cash register/Coin
   PAY_TOLL: 'https://assets.mixkit.co/active_storage/sfx/228/228-preview.mp3', // Coin drop
   TURN_START: 'https://assets.mixkit.co/active_storage/sfx/2568/2568-preview.mp3', // Notification
-  ERROR: 'https://assets.mixkit.co/active_storage/sfx/2573/2573-preview.mp3', // Error beep
+  ERROR: 'https://assets.dpsteam.xyz/gridx-assets/bell.mp3', // Error beep
 };
 
 const audioCache: Record<string, HTMLAudioElement> = {};
