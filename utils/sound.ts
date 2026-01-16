149e63824e5c557de7a3303b3bd38fe4ed962d64
HandsomeBisu <joscar12@daum.net>
2026-01-16 19:51:15 +0900

    Add files via upload

--- utils/sound.ts
+++ utils/sound.ts
@@ -3,7 +3,7 @@ export const SOUNDS = {
   CLICK: 'https://assets.dpsteam.xyz/gridx-assets/tick.mp3', // Pop click
   DICE_ROLL: 'https://assets.dpsteam.xyz/gridx-assets/dice.mp3', // Rolling
   MOVE: 'https://assets.dpsteam.xyz/gridx-assets/tick.mp3', // Short tick
-  BUILD: 'https://assets.mixkit.co/active_storage/sfx/95/95-preview.mp3', // Cash register/Coin
+  BUILD: 'https://assets.dpsteam.xyz/gridx-assets/getmoney2.mp3', // Cash register/Coin
   PAY_TOLL: 'https://assets.mixkit.co/active_storage/sfx/228/228-preview.mp3', // Coin drop
   TURN_START: 'https://assets.mixkit.co/active_storage/sfx/2568/2568-preview.mp3', // Notification
   ERROR: 'https://assets.dpsteam.xyz/gridx-assets/bell.mp3', // Error beep
